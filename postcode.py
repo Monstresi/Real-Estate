87a6a42004321b157b639a66f90455399c2872be
sali3678 <sali3678@uni.sydney.edu.au>
2023-08-29 16:31:42 +1000

    Added length verification

--- postcode.py
+++ postcode.py
@@ -1,3 +1,15 @@
-postcode = input("Enter your postcode: ")
 
-print(f"Your postcode is {postcode}")
+def get_postcode():
+    postcode = input("Enter your postcode: ")
+    return postcode
+
+def main():
+    postcode = get_postcode()
+    while len(postcode) != 4:
+        postcode = get_postcode()
+
+    print(f"Your postcode is {postcode}")
+    exit()
+
+
+main()
